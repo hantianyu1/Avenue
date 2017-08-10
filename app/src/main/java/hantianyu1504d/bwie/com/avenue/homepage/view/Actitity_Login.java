7dcf992f5bd77faa8c61ece963b765252e1c7299
lichaohuilala <578078578@qq.com>
2017-08-10 20:32:54 +0800

    主代码的 完成

---
+++ app/src/main/java/hantianyu1504d/bwie/com/avenue/homepage/view/Actitity_Login.java
@@ -0,0 +1,25 @@
+package hantianyu1504d.bwie.com.avenue.homepage.view;
+
+import hantianyu1504d.bwie.com.avenue.R;
+import hantianyu1504d.bwie.com.avenue.base.BaseActvitiy;
+
+/**
+ * Created by lichaohui on 2017/8/10.
+ */
+
+public class Actitity_Login extends BaseActvitiy {
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int initContentView() {
+        return R.layout.activity_login;
+    }
+}
