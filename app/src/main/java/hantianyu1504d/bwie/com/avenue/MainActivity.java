c4e9b060baec519eb18b7d362d23bcbe4407559a
wangwenjie <1975098810@qq.com>
2017-08-10 17:32:40 +0800

    王文杰第一次提交

--- app/src/main/java/hantianyu1504d/bwie/com/avenue/MainActivity.java
+++ app/src/main/java/hantianyu1504d/bwie/com/avenue/MainActivity.java
@@ -16,7 +16,7 @@ import hantianyu1504d.bwie.com.avenue.nearby.view.NearbyFragment;
 import hantianyu1504d.bwie.com.avenue.rebate.view.RebateFragment;
 
 public class MainActivity extends AppCompatActivity {
-//山东高速告诉交话费
+//山东高速告诉交话费黄飞鸿
     @BindView(R.id.main_home)
     RadioButton mainHome;
     @BindView(R.id.main_nearby)
