ac953e74bb759ba0d5783a58e93d9dd7d6975696
liujie <862334111@qq.com>
2017-08-11 15:26:14 +0800

    刘婕提交布局代码

--- app/src/main/java/hantianyu1504d/bwie/com/avenue/rebate/view/RebateFragment.java
+++ app/src/main/java/hantianyu1504d/bwie/com/avenue/rebate/view/RebateFragment.java
@@ -2,10 +2,15 @@ package hantianyu1504d.bwie.com.avenue.rebate.view;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import hantianyu1504d.bwie.com.avenue.R;
 
@@ -15,6 +20,8 @@ import hantianyu1504d.bwie.com.avenue.R;
  */
 public class RebateFragment extends Fragment {
 
+
+    private TextView mTxt;
 
     public RebateFragment() {
         // Required empty public constructor
@@ -28,4 +35,18 @@ public class RebateFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_rebate, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SpannableString spannableString = new SpannableString("5月25日（明天）返利230元");
+        RelativeSizeSpan sizeSpan01 = new RelativeSizeSpan(1.6f);
+        spannableString.setSpan(sizeSpan01, 11, 14, Spanned.SPAN_INCLUSIVE_INCLUSIVE );
+        mTxt.setText(spannableString);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTxt = (TextView) view.findViewById(R.id.txt_230);
+    }
 }
