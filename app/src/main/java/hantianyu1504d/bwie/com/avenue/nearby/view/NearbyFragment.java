4be325bb4b71347519fc73e3da58e2f935e2d52c
liujie <862334111@qq.com>
2017-08-12 14:15:46 +0800

    刘婕提交

--- app/src/main/java/hantianyu1504d/bwie/com/avenue/nearby/view/NearbyFragment.java
+++ app/src/main/java/hantianyu1504d/bwie/com/avenue/nearby/view/NearbyFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationConfiguration;
-
 import hantianyu1504d.bwie.com.avenue.R;
 
 
@@ -23,9 +18,6 @@ import hantianyu1504d.bwie.com.avenue.R;
 public class NearbyFragment extends Fragment {
 
     private View view;
-    private MapView map;
-    private BaiduMap baiduMap;
-    private MyLocationConfiguration.LocationMode mCurrentMode;
 
     public NearbyFragment() {
         // Required empty public constructor
@@ -34,18 +26,8 @@ public class NearbyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        SDKInitializer.initialize(getActivity().getApplicationContext());
         view = inflater.inflate(R.layout.fragment_nearby, container, false);
-        initView();
         return view;
-    }
-
-    private void initView() {
-        map = (MapView) view.findViewById(R.id.nearby_map);
-        baiduMap = map.getMap();
-        //普通地图
-        baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
-
     }
 
     @Override
