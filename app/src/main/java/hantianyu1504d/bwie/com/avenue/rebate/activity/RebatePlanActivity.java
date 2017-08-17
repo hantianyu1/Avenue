e15553fb691886f914d8d83ccafbcd13c4128c77
liujie <862334111@qq.com>
2017-08-17 21:48:06 +0800

    刘婕返利计划条目点击

--- app/src/main/java/hantianyu1504d/bwie/com/avenue/rebate/activity/RebatePlanActivity.java
+++ app/src/main/java/hantianyu1504d/bwie/com/avenue/rebate/activity/RebatePlanActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,16 +22,26 @@ import hantianyu1504d.bwie.com.avenue.R;
 public class RebatePlanActivity extends AppCompatActivity {
     @BindView(R.id.img_return)
     ImageView imgReturn;
+    @BindView(R.id.get_recordCoding)
+    TextView getRecordCoding;
+    @BindView(R.id.get_cashbackSpecificDate)
+    TextView getCashbackSpecificDate;
+    @BindView(R.id.get_IntegralStyle)
+    ImageView getIntegralStyle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rebateplan);
         ButterKnife.bind(this);
-
-
+        Intent intent = getIntent();
+        String recordCoding = intent.getStringExtra("recordCoding");
+        String cashbackSpecificDate = intent.getStringExtra("cashbackSpecificDate");
+        String integralStyle = intent.getStringExtra("IntegralStyle");
+        getRecordCoding.setText(recordCoding);
+        getCashbackSpecificDate.setText(cashbackSpecificDate);
+        Glide.with(this).load(integralStyle).into(getIntegralStyle);
     }
-
 
 
     @OnClick(R.id.img_return)
