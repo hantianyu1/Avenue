6f42fd0f383af37af4db256756c46985907cc4c6
lichaohuilala <578078578@qq.com>
2017-08-25 20:40:43 +0800

    主页 返回两次退出

--- app/src/main/java/hantianyu1504d/bwie/com/avenue/MainActivity.java
+++ app/src/main/java/hantianyu1504d/bwie/com/avenue/MainActivity.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import hantianyu1504d.bwie.com.avenue.application.MyApplication;
 import hantianyu1504d.bwie.com.avenue.homepage.view.HomeFragment;
 import hantianyu1504d.bwie.com.avenue.mine.view.fragment.MineFragment;
 import hantianyu1504d.bwie.com.avenue.nearby.view.NearbyFragment;
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private NearbyFragment nearby;
     private RebateFragment rebate;
     private MineFragment mine;
+    private boolean isExit;
 
     private List<Fragment> fragments;
 
@@ -123,4 +129,26 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
         fg = null;
     }
+
+    //单击返回键两次退出
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (isExit == false) {
+                isExit = true;
+                Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        isExit = false;
+                    }
+                }, 2000);
+            } else {
+                MyApplication.getInstance().exitApp();
+            }
+        }
+        return false;
+    }
+
+
 }
